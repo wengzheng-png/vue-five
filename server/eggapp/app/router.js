a2e56d06f327185eba1151b736c77e5551ed10a7
e <2420244898@qq.com>
2020-12-21 15:01:37 +0800

    最后一次修改框架

--- server/eggapp/app/router.js
+++ server/eggapp/app/router.js
@@ -6,20 +6,6 @@
 module.exports = app => {
   const { router, controller } = app;
 
-
-
-
   router.get('/homegoods', controller.home.homegoods);//首页的商品 默认50条
-
-  router.post('/register', controller.home.register);//注册的页面
-
-  router.post('/login', controller.home.login);//登录的页面
-
-  router.get('/car', controller.home.car);//用户购物车的接口
-
-  router.get('/addgoodstocar', controller.home.addgoodstocar);//添加购物车接口
-
-  router.get('/userinfo', controller.home.userinfo);//获得用户的信息
-
  
 };
