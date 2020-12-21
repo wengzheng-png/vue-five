d17dd8ec58e7aa1cdac84adcb96884a7c44f6bbb
e <2420244898@qq.com>
2020-12-21 15:41:21 +0800

    ui的引入

--- client/init/src/main.js
+++ client/init/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store' //全局引入 vueX
 import Axios from 'axios'  //全局引入 axios
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';//ui框架的引入
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = Axios
-
+Vue.use(ElementUI); //ui的注入
 new Vue({
   router,
   store,
